5d0183cc551b73554666e3ab0abdc05b1e9d1fbb
meramiha <62883073+meramiha@users.noreply.github.com>
2024-01-23 15:57:47 +0100

    remove m_type

--- any.cpp
+++ any.cpp
@@ -6,11 +6,6 @@ namespace utils {
         return "";
     }
 
-
-    [[nodiscard]] std::string any::to_string() const noexcept {
-        return m_type;
-    }
-
     [[nodiscard]] bool any::empty() const noexcept {
         return this->m_value == nullptr;
     }
--- any.h
+++ any.h
@@ -2,6 +2,7 @@
 #define ANY_H
 #include <string>
 #include <typeinfo>
+#include <memory>
 
 namespace utils {
     class any {
@@ -11,7 +12,9 @@ namespace utils {
 
             virtual ~any_storage_base() = default;
 
-            virtual std::unique_ptr<any_storage_base> copy_data() = 0;
+            virtual std::type_info const &get_type() = 0;
+
+            virtual std::unique_ptr<any_storage_base> get_unique_pointer_to_copy() = 0;
         };
 
         template<typename T>
@@ -22,11 +25,15 @@ namespace utils {
             explicit any_storage_derived(T obj): m_ptr(obj) {
             }
 
+            std::type_info const &get_type() override {
+                return typeid(T);
+            }
+
             void *get_pointer() override {
                 return static_cast<void *>(&this->m_ptr);
             }
 
-            std::unique_ptr<any_storage_base> copy_data() override {
+            std::unique_ptr<any_storage_base> get_unique_pointer_to_copy() override {
                 return std::make_unique<any_storage_derived<T> >(m_ptr);
             }
 
@@ -35,62 +42,46 @@ namespace utils {
 
 
         std::unique_ptr<any_storage_base> m_value = nullptr;
-        std::string m_type;
 
     public:
         any() = default;
 
         template<typename T>
-        explicit any(T value): m_value(any_storage_derived<T>(value).copy_data()),
-                               m_type(typeid(T).name()) {
+        explicit any(T value): m_value(any_storage_derived<T>(value).get_unique_pointer_to_copy()) {
             static_assert(std::is_copy_constructible_v<T>);
         }
 
 
         template<typename T>
         any &operator=(T value) {
-            // delete this->m_value;
-
             this->m_value.reset();
-
-            // NOLINTNEXTLINE
-            m_value = any_storage_derived<T>(value).copy_data();
-            m_type = typeid(T).name();
+            m_value = any_storage_derived<T>(value).get_unique_pointer_to_copy();
 
             return *this;
         }
 
         any(any const &other) {
             this->m_value.reset();
-
-            this->m_value = other.m_value->copy_data();
-
-            this->m_type = other.m_type; // NOLINT
+            this->m_value = other.m_value->get_unique_pointer_to_copy();
         }
 
         any &operator=(any const &other) {
             this->m_value.reset();
-
-            this->m_value = other.m_value->copy_data();
-
-            this->m_type = other.m_type; // NOLINT
+            this->m_value = other.m_value->get_unique_pointer_to_copy();
 
             return *this;
         }
 
-        any(any &&other) noexcept : m_value(std::move(other.m_value)), m_type(std::move(other.m_type)) {
+        any(any &&other) noexcept : m_value(std::move(other.m_value)) {
         }
 
         any &operator=(any &&other) noexcept {
             m_value = std::move(other.m_value);
-            m_type = std::move(other.m_type);
 
             return *this;
         }
 
         [[nodiscard]] bool empty() const noexcept;
-
-        [[nodiscard]] std::string to_string() const noexcept;
 
         template<typename T>
         friend T *any_cast(any *object);
@@ -98,7 +89,7 @@ namespace utils {
         template<typename T>
         friend T any_cast(any &object);
 
-        ~any() =default;
+        ~any() = default;
     };
 
     class bad_any_cast final : std::bad_cast {
@@ -108,7 +99,7 @@ namespace utils {
 
     template<typename T>
     T *any_cast(any *object) {
-        if (object->m_type != typeid(T).name()) {
+        if (object->m_value->get_type() != typeid(T)) {
             return nullptr;
         }
 
@@ -117,7 +108,7 @@ namespace utils {
 
     template<typename T>
     T any_cast(any &object) {
-        if (object.m_type != typeid(T).name()) {
+        if (object.m_value->get_type() != typeid(T)) {
             throw bad_any_cast();
         }
 
