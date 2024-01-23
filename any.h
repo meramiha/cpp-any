4727781551180ad35c65b2611eb4898fb7a34894
meramiha <62883073+meramiha@users.noreply.github.com>
2024-01-23 15:45:25 +0100

    something with more unique pointers

--- any.h
+++ any.h
@@ -4,60 +4,86 @@
 #include <typeinfo>
 
 namespace utils {
-    class any_storage_base {
-    public:
-        virtual void *get_pointer() = 0;
-
-        virtual ~any_storage_base() = 0;
-    };
-
-    template<typename T>
-    class any_storage_derived : public any_storage_base {
-        T m_ptr;
-
-    public:
-        // explicit any_storage_derived(T *ptr);
-
-        explicit any_storage_derived(T obj): m_ptr(obj) {
-        }
-
-        void *get_pointer() override {
-            return static_cast<void *>(&this->m_ptr);
-        }
-        // explicit any_storage_derived(any_storage_derived const& other){
-        //     this->m_ptr = other.m_ptr;
-        //
-        // }
-
-        ~any_storage_derived() override {
-            // delete m_ptr;
-        }
-    };
-
     class any {
-        // void *m_value = nullptr;
+        class any_storage_base {
+        public:
+            virtual void *get_pointer() = 0;
 
-        any_storage_base *m_value = nullptr;
+            virtual ~any_storage_base() = default;
+
+            virtual std::unique_ptr<any_storage_base> copy_data() = 0;
+        };
+
+        template<typename T>
+        class any_storage_derived final : public any_storage_base {
+            T m_ptr;
+
+        public:
+            explicit any_storage_derived(T obj): m_ptr(obj) {
+            }
+
+            void *get_pointer() override {
+                return static_cast<void *>(&this->m_ptr);
+            }
+
+            std::unique_ptr<any_storage_base> copy_data() override {
+                return std::make_unique<any_storage_derived<T> >(m_ptr);
+            }
+
+            ~any_storage_derived() override = default;
+        };
+
+
+        std::unique_ptr<any_storage_base> m_value = nullptr;
         std::string m_type;
 
     public:
         any() = default;
 
-        // any(const any &other) = default;
-
-        // any &operator=(const any &other) = default;
-
         template<typename T>
-        explicit any(T const &value): m_value(new any_storage_derived<T>(value)),
-                                      m_type(typeid(T).name()) {
+        explicit any(T value): m_value(any_storage_derived<T>(value).copy_data()),
+                               m_type(typeid(T).name()) {
+            static_assert(std::is_copy_constructible_v<T>);
         }
 
-        template<typename T>
-        any &operator=(T const &value) {
-            // delete m_value;
 
-            m_value = new any_storage_derived<T>(value);
+        template<typename T>
+        any &operator=(T value) {
+            // delete this->m_value;
+
+            this->m_value.reset();
+
+            // NOLINTNEXTLINE
+            m_value = any_storage_derived<T>(value).copy_data();
             m_type = typeid(T).name();
+
+            return *this;
+        }
+
+        any(any const &other) {
+            this->m_value.reset();
+
+            this->m_value = other.m_value->copy_data();
+
+            this->m_type = other.m_type; // NOLINT
+        }
+
+        any &operator=(any const &other) {
+            this->m_value.reset();
+
+            this->m_value = other.m_value->copy_data();
+
+            this->m_type = other.m_type; // NOLINT
+
+            return *this;
+        }
+
+        any(any &&other) noexcept : m_value(std::move(other.m_value)), m_type(std::move(other.m_type)) {
+        }
+
+        any &operator=(any &&other) noexcept {
+            m_value = std::move(other.m_value);
+            m_type = std::move(other.m_type);
 
             return *this;
         }
@@ -72,7 +98,7 @@ namespace utils {
         template<typename T>
         friend T any_cast(any &object);
 
-        ~any();
+        ~any() =default;
     };
 
     class bad_any_cast final : std::bad_cast {
