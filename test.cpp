e0f4ed860b6eac31c560e39a9329b64659bb20de
meramiha <62883073+meramiha@users.noreply.github.com>
2024-01-25 13:58:04 +0100

    static asserts for any_cast return type

--- test.cpp
+++ test.cpp
@@ -1,14 +1,17 @@
+#include <any>
 #include <cassert>
-#include <string>
+#include <cstring>
 #include <iostream>
+#include <string>
 #include "any.h"
+
+using std::cerr;
+using std::cout;
+using std::endl;
+using std::string;
 using utils::any;
 using utils::any_cast;
 using utils::bad_any_cast;
-using std::string;
-using std::cout;
-using std::cerr;
-using std::endl;
 
 void contruct_test() {
     any def;
@@ -21,7 +24,7 @@ void contruct_test() {
     assert(e.empty());
 }
 
-template<class T>
+template <class T>
 void check_cast(any &a, bool should_throw) {
     bool thrown = false;
     try {
@@ -46,8 +49,6 @@ void swap_test(any &a, any &b) {
     std::swap(a, b);
 }
 
-
-
 void my_empty_test() {
     any a;
     assert(a.empty());
@@ -57,16 +58,107 @@ void my_empty_test() {
     assert(!a.empty());
 }
 
-
 void my_pointer_test() {
-
     int obj = 1;
 
     int *ptr = &obj;
 
     any a(ptr);
 
+    int *h = new int();
 
+    any b(h);
+
+    any c(*h);
+
+    delete h;
+}
+
+void my_value_test() {
+    double k = 2.39;
+
+    any a(k);
+
+    assert(k == any_cast<double>(a));
+    assert(0.0 != any_cast<double>(a));
+    int i = 155;
+    a = i;
+
+    assert(i == any_cast<int>(a));
+
+    a = 4;
+    assert(4 == any_cast<int>(a));
+    assert(5 != any_cast<int>(a));
+}
+
+void my_non_trivially_copyable_test() {
+    std::string s =
+        "my really "
+        "looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo"
+        "ooooooooooooooooooooooooooooong string";
+
+    std::string same_string =
+        "my really "
+        "looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo"
+        "ooooooooooooooooooooooooooooong string";
+    any a(s);
+
+    static_assert(!std::is_trivially_copyable_v<std::string>);
+    assert(same_string == any_cast<std::string>(a));
+}
+
+void my_custom_class_test() {  // this test only make sense with valgrind
+    class B {
+        // char *ptr;
+        int *ptr = nullptr;
+
+    public:
+        B() {
+            // ptr = new char[1000];
+            ptr = new int;
+        }
+
+        B(B const &other) {
+            ptr = new int;
+            std::memcpy(ptr, other.ptr, sizeof(int));
+        }
+
+        ~B() {
+            delete ptr;
+
+            std::cerr << "Destructor called" << std::endl;
+        }
+    };
+
+    static_assert(std::is_copy_constructible_v<B>);
+
+    any a{B()};
+
+    a = 1;
+}
+
+void my_const_test() {
+    static_assert(
+        std::is_same_v<const int &, decltype(any_cast<const int &>(any(1)))>);
+
+    static_assert(std::is_same_v<int &, decltype(any_cast<int &>(any(1)))>);
+
+    static_assert(std::is_same_v<int, decltype(any_cast<int>(any(1)))>);
+
+    any a(0);
+
+    static_assert(
+        std::is_same_v<const int &, decltype(any_cast<const int &>(a))>);
+
+    static_assert(std::is_same_v<int &, decltype(any_cast<int &>(a))>);
+
+    static_assert(std::is_same_v<int, decltype(any_cast<int>(a))>);
+
+    static_assert(std::is_same_v<int *, decltype(any_cast<int>(&a))>);
+
+    any const_a(0);
+    static_assert(
+        std::is_same_v<const int *, decltype(any_cast<const int>(&const_a))>);
 }
 
 int main() {
@@ -76,5 +168,16 @@ int main() {
     retrieve_value_test();
     any a(5), b(string("6"));
     swap_test(a, b);
+
+    // my tests
+    my_empty_test();
+    my_pointer_test();
+    my_value_test();
+    my_non_trivially_copyable_test();
+    my_custom_class_test();
+
+    my_const_test();
+
+
     return 0;
 }
